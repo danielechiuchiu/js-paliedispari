4f45bfd3315c11b60fd6f85ade4a43a8939ee4a4
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-15 16:53:22 +0200

    commit

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// prompt di una parola e variabile risultato
 var input = prompt('Inserisci una parola');
 var risultato = 'Ritenta';
 
-
+// richiamo la funzione e sovrascrivo il risultato se si verifica la condizione
 if (input == verifica(input)){
   risultato = 'L hai trovata';
 }
 
+//stampo il risultato finale
 document.getElementById('esercizio').innerHTML = risultato;
 
+// costruisco la funzione di verifica dell' ordine delle lettere delle parole partendo dall' ultima a descrescere
+// creo una variabile 'muta' che rimane dentro la funzione e che ci da la parola scritta al contrario
 function verifica(palindromo) {
     var controllo = '';
     for (var i = palindromo.length - 1; i >= 0; i--) {
         controllo = controllo + palindromo[i];
     }
     return controllo;
-    console.log('palindromo')
 }
