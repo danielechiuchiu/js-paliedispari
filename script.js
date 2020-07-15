83927f479aad7d3a00fd4f25b1d34d31e28bf4ec
danielechiuchiu <daniele.chiuchiu@gmail.com>
2020-07-15 19:46:49 +0200

    commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // prompt di una parola e variabile risultato
 var input = prompt('Inserisci una parola');
 var risultato = 'Ritenta';
+input = input.toLowerCase();
 
 // richiamo la funzione e sovrascrivo il risultato se si verifica la condizione
 if (input == verifica(input)){
-  risultato = 'L hai trovata';
+  risultato = 'l\' hai trovata';
 }
 
 //stampo il risultato finale
-document.getElementById('esercizio').innerHTML = risultato;
+document.getElementById('esercizio').innerHTML = 'La parola che hai inserito è' + ' ' + input + ' :'+ ' ' + 'quindi' + ' ' + risultato;
 
 // costruisco la funzione di verifica dell' ordine delle lettere delle parole partendo dall' ultima a descrescere
 // creo una variabile 'muta' che rimane dentro la funzione e che ci da la parola scritta al contrario
